ad95e63d0bcb5e9320b2f6eb438686abd3230a69
rafalb <rafalbarszczewski85@gmail.com>
2019-05-04 15:16:03 +0200

    Quicksort implementation

---
+++ Algorithms/Sorting/Quicksorter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Sorting
+{
+    public class Quicksorter<T> where T : IComparable
+    {
+        public void Quicksort(T[] items)
+        {
+            Quicksort(items, 0, items.Length - 1);
+        }
+
+        private void Quicksort(T[] items, int startIndex, int endIndex)
+        {
+            if (endIndex > startIndex)
+            {
+                int splitIndex = Split(items, startIndex, endIndex);
+
+                Quicksort(items, startIndex, splitIndex - 1);
+                Quicksort(items, splitIndex + 1, endIndex);
+            }
+        }
+
+        private int Split(T[] items, int startIndex, int endIndex)
+        {
+            int splitIndex = startIndex;
+            T splitItem = items[endIndex];
+
+            for (int i = startIndex; i < endIndex; i++)
+            {
+                if (Comparer<T>.Default.Compare(items[i], splitItem) < 0)
+                {
+                    Swap(items, splitIndex, i);
+                    splitIndex++;
+                }
+            }
+
+            Swap(items, splitIndex, endIndex);
+
+            return splitIndex;
+        }
+
+        private void Swap(T[] items, int index1, int index2)
+        {
+            T item = items[index1];
+            items[index1] = items[index2];
+            items[index2] = item;
+        }
+    }
+}
---
+++ Algorithms/Sorting/QuicksorterTests.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Sorting
+{
+    [TestFixture]
+    internal class QuicksorterTests
+    {
+        [Test]
+        public void ShouldSortEmptyArray()
+        {
+            var sorter = new Quicksorter<int>();
+
+            int[] items = { };
+            sorter.Quicksort(items);
+
+            Assert.IsEmpty(items);
+        }
+
+        [Test]
+        public void ShouldSortTwoItems()
+        {
+            var sorter = new Quicksorter<int>();
+
+            int[] items = { 8, 3 };
+            sorter.Quicksort(items);
+
+            Assert.AreEqual(new int[] { 3, 8 }, items);
+        }
+
+        [Test]
+        public void ShouldSortMultipleItems()
+        {
+            var sorter = new Quicksorter<int>();
+
+            int[] items = { 8, 3, 4, 12, 73, 1, 3, 4, 5 };
+            sorter.Quicksort(items);
+
+            Assert.AreEqual(new int[] { 1, 3, 3, 4, 4, 5, 8, 12, 73 }, items);
+        }
+    }
+}
