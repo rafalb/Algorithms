df0eda4ffdffbfd54239e27f5d14022cc53850ef
rafalb <rafalbarszczewski85@gmail.com>
2019-05-05 19:01:59 +0200

    Counting sort: mock implementation

---
+++ Algorithms/Sorting/CountingSorter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Sorting
+{
+    public class CountingSorter
+    {
+        public int[] CountingSort(int[] items, int maxValue)
+        {
+            var sortedItems = new int[items.Length];
+
+            return sortedItems;
+        }
+    }
+}
---
+++ Algorithms/Sorting/Tests/CountingSorterTests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Sorting.Tests
+{
+    [TestFixture]
+    internal class CountingSorterTests
+    {
+        [Test]
+        public void ShouldSortEmptyArray()
+        {
+            var sorter = new CountingSorter();
+            int[] result = sorter.CountingSort(new int[] { }, 5);
+
+            Assert.IsEmpty(result);
+        }
+
+        [Test]
+        public void ShouldSortSingleItem()
+        {
+            var sorter = new CountingSorter();
+            int[] result = sorter.CountingSort(new int[] { 3 }, 5);
+
+            Assert.AreEqual(3, result[0]);
+        }
+
+        [Test]
+        public void ShouldCreateSameSizedResult()
+        {
+            var sorter = new CountingSorter();
+            int[] result = sorter.CountingSort(new int[] { 3, 1, 5, 4 }, 5);
+
+            Assert.AreEqual(4, result.Length);
+        }
+    }
+}
