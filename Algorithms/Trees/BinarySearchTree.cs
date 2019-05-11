af5008366eab74544847dbc532deaed27b6ba1c0
rafalb <rafalbarszczewski85@gmail.com>
2019-05-11 13:48:54 +0200

    Binary Search Tree (BST) implementation

---
+++ Algorithms/Trees/BinarySearchTree.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Trees
+{
+    public class BinarySearchTree<T> where T : IComparable
+    {
+        private class Node
+        {
+            public Node(Node parent, T value)
+            {
+                this.parent = parent;
+                Value = value;
+            }
+
+            private Node parent;
+            private Node leftSon;
+            private Node rightSon;
+
+            public T Value { get; private set; }
+
+            public override string ToString()
+            {
+                return Value.ToString();
+            }
+
+            public void Insert(T newValue)
+            {
+                if (Comparer<T>.Default.Compare(newValue, Value) < 0)
+                {
+                    if (leftSon == null)
+                    {
+                        leftSon = new Node(this, newValue);
+                    }
+                    else
+                    {
+                        leftSon.Insert(newValue);
+                    }
+                }
+                else
+                {
+                    if (rightSon == null)
+                    {
+                        rightSon = new Node(this, newValue);
+                    }
+                    else
+                    {
+                        rightSon.Insert(newValue);
+                    }
+                }
+            }
+
+            public void AddSortedValues(T[] values, ref int index)
+            {
+                if (leftSon != null)
+                {
+                    leftSon.AddSortedValues(values, ref index);
+                }
+
+                values[index] = Value;
+                index++;
+
+                if (rightSon != null)
+                {
+                    rightSon.AddSortedValues(values, ref index);
+                }
+            }
+
+            public bool Contains(T value)
+            {
+                Node node = this;
+
+                while (node != null)
+                {
+                    int comparison = Comparer<T>.Default.Compare(value, node.Value);
+
+                    if (comparison == 0)
+                    {
+                        return true;
+                    }
+                    else if (comparison < 0)
+                    {
+                        node = node.leftSon;
+                    }
+                    else
+                    {
+                        node = node.rightSon;
+                    }
+                }
+
+                return false;
+            }
+
+            public void Remove(T value, ref Node newRoot)
+            {
+                int comparison = Comparer<T>.Default.Compare(value, Value);
+
+                if (comparison == 0)
+                {
+                    Node formerLeftSon = leftSon;
+                    Node formerRightSon = rightSon;
+
+                    RemoveSelf();
+
+                    if (parent == null)
+                    {
+                        if (formerLeftSon != null && formerRightSon != null)
+                        {
+                            newRoot = formerLeftSon.parent;
+                        }
+                        else if (formerLeftSon != null)
+                        {
+                            newRoot = formerLeftSon;
+                        }
+                        else
+                        {
+                            newRoot = formerRightSon;
+                        }
+                    }
+                }
+                else if (comparison < 0)
+                {
+                    if (leftSon != null)
+                    {
+                        leftSon.Remove(value, ref newRoot);
+                    }
+                }
+                else
+                {
+                    if (rightSon != null)
+                    {
+                        rightSon.Remove(value, ref newRoot);
+                    }
+                }
+            }
+
+            private void RemoveSelf()
+            {
+                Node newSon;
+
+                if (leftSon != null && rightSon != null)
+                {
+                    newSon = rightSon;
+
+                    while (newSon.leftSon != null)
+                    {
+                        newSon = newSon.leftSon;
+                    }
+
+                    if (newSon.parent.leftSon == newSon)
+                    {
+                        newSon.parent.leftSon = newSon.rightSon;
+
+                        if (newSon.rightSon != null)
+                        {
+                            newSon.rightSon.parent = newSon.parent;
+                        }
+                    }
+
+                    newSon.leftSon = leftSon;
+                    leftSon.parent = newSon;
+
+                    if (newSon.parent != this)
+                    {
+                        newSon.rightSon = rightSon;
+                        rightSon.parent = newSon;
+                    }
+                }
+                else
+                {
+                    newSon = leftSon != null ? leftSon : rightSon;
+                }
+
+                if (parent != null)
+                {
+                    if (parent.leftSon == this)
+                    {
+                        parent.leftSon = newSon;
+                    }
+                    else
+                    {
+                        parent.rightSon = newSon;
+                    }
+                }
+
+                if (newSon != null)
+                {
+                    newSon.parent = parent;
+                }
+            }
+        }
+
+        private Node root;
+
+        public int NodeCount { get; private set; }
+
+        public T RootValue
+        {
+            get { return root.Value; }
+        }
+
+        public void Add(T value)
+        {
+            NodeCount += 1;
+
+            if (root == null)
+            {
+                root = new Node(null, value);
+            }
+            else
+            {
+                root.Insert(value);
+            }
+        }
+
+        public void Add(params T[] values)
+        {
+            foreach (var value in values)
+            {
+                Add(value);
+            }
+        }
+
+        public void Remove(T value)
+        {
+            if (Contains(value))
+            {
+                NodeCount -= 1;
+            }
+
+            if (root != null)
+            {
+                root.Remove(value, ref root);
+            }
+        }
+
+        public T[] GetSortedValues()
+        {
+            var sortedValues = new T[NodeCount];
+
+            if (root != null)
+            {
+                int index = 0;
+                root.AddSortedValues(sortedValues, ref index);
+            }
+
+            return sortedValues;
+        }
+
+        public bool Contains(T value)
+        {
+            return root == null ? false : root.Contains(value);
+        }
+    }
+}
---
+++ Algorithms/Trees/Tests/BinarySearchTreeTests.cs
@@ -0,0 +1,106 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Trees.Tests
+{
+    [TestFixture]
+    internal class BinarySearchTreeTests
+    {
+        private BinarySearchTree<int> tree;
+
+        [SetUp]
+        public void SetUp()
+        {
+            tree = new BinarySearchTree<int>();
+        }
+
+        [Test]
+        public void ShouldCreateEmptyTree()
+        {
+            Assert.AreEqual(0, tree.NodeCount);
+        }
+
+        [Test]
+        public void ShouldAddItemToEmptyTree()
+        {
+            tree.Add(5);
+            Assert.AreEqual(1, tree.NodeCount);
+        }
+
+        [Test]
+        public void ShouldMakeItemAddedToEmptyTreeTheRoot()
+        {
+            tree.Add(5);
+            Assert.AreEqual(5, tree.RootValue);
+        }
+
+        [Test]
+        public void ShouldMaintainNodeCountWhenAddingItems()
+        {
+            tree.Add(5, 3, 8, 2, 5);
+            Assert.AreEqual(5, tree.NodeCount);
+        }
+
+        [Test]
+        public void ShouldReturnTwoSortedItems()
+        {
+            tree.Add(5, 3);
+            int[] sortedValues = tree.GetSortedValues();
+            Assert.AreEqual(new int[] { 3, 5 }, sortedValues);
+        }
+
+        [Test]
+        public void ShouldSortMultipleItems()
+        {
+            tree.Add(5, 3, 1, 8, 5, 3, 4, 5, 1);
+            int[] sortedValues = tree.GetSortedValues();
+            Assert.AreEqual(new int[] { 1, 1, 3, 3, 4, 5, 5, 5, 8 }, sortedValues);
+        }
+
+        [Test]
+        public void ShouldFindItemAddedToTree()
+        {
+            tree.Add(5, 3, 1, 8, 5, 3, 4, 5, 1);
+            Assert.IsTrue(tree.Contains(4));
+        }
+
+        [Test]
+        public void ShouldNotFindItemNotAddedToTree()
+        {
+            tree.Add(5, 3, 1, 8, 5, 3, 4, 5, 1);
+            Assert.IsFalse(tree.Contains(6));
+        }
+
+        [Test]
+        public void ShouldRemoveItemFromTree()
+        {
+            tree.Add(5, 3, 1, 8, 5, 3, 4, 5, 1);
+            tree.Remove(4);
+            Assert.IsFalse(tree.Contains(4));
+        }
+
+        [Test]
+        public void ShouldKeepSortedOrderAfterRemovingItem()
+        {
+            tree.Add(15, 5, 16, 3, 12, 20, 10, 13, 18, 23, 6, 7);
+            tree.Remove(6);
+
+            int[] sortedValues = tree.GetSortedValues();
+            Assert.AreEqual(new int[] { 3, 5, 7, 10, 12, 13, 15, 16, 18, 20, 23}, sortedValues);
+        }
+
+        [Test]
+        public void ShouldKeepSortedOrderAfterRemovingRoot()
+        {
+            tree.Add(15, 5, 16, 3, 12, 20, 10, 13, 18, 23, 6, 7);
+            tree.Remove(15);
+
+            int[] sortedValues = tree.GetSortedValues();
+            Assert.AreEqual(new int[] { 3, 5, 6, 7, 10, 12, 13, 16, 18, 20, 23 }, sortedValues);
+        }
+    }
+}
