77cc8f0918d86e5617686293dd171d7ff9eea9ef
rafalb <rafalbarszczewski85@gmail.com>
2019-05-11 15:33:13 +0200

    Longest common subsequence - recursive solution with dynamic programming

---
+++ Algorithms/DynamicProgramming/LongestCommonSubsequence.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.DynamicProgramming
+{
+    public class LongestCommonSubsequence
+    {
+        public string FindLongestCommonSubsequence(string s1, string s2)
+        {
+            if (String.IsNullOrEmpty(s1) || String.IsNullOrEmpty(s2))
+            {
+                return String.Empty;
+            }
+            else
+            {
+                if (s1.Last() == s2.Last())
+                {
+                    return FindLongestCommonSubsequence(s1.Substring(0, s1.Length - 1), s2.Substring(0, s2.Length - 1)) + s1.Last();
+                }
+                else
+                {
+                    string result1 = FindLongestCommonSubsequence(s1.Substring(0, s1.Length - 1), s2.Substring(0, s2.Length));
+                    string result2 = FindLongestCommonSubsequence(s1.Substring(0, s1.Length), s2.Substring(0, s2.Length - 1));
+
+                    return result1.Length > result2.Length ? result1 : result2;
+                }
+            }
+        }
+    }
+}
