9c87f1379d5e0fd4ea229c82b4f35b7ddf488d04
rafalb <rafalbarszczewski85@gmail.com>
2019-05-19 13:10:48 +0200

    Graph topological sorting implemented

---
+++ Algorithms/Graphs/Tests/TopologicalSorterTests.cs
@@ -0,0 +1,61 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Graphs.Tests
+{
+    [TestFixture]
+    internal class TopologicalSorterTests
+    {
+        private TopologicalSorter sorter;
+
+        [SetUp]
+        public void SetUp()
+        {
+            sorter = new TopologicalSorter();
+        }
+
+        [Test]
+        public void ShouldGiveEmptyResultForEmptyGraph()
+        {
+            var nodes = sorter.TopologicalSort(new Graph<int>());
+
+            Assert.IsEmpty(nodes);
+        }
+
+        [Test]
+        public void ShouldSortSingleNodeGraph()
+        {
+            var graph = new Graph<int>();
+            var node = graph.AddNode(5);
+            var nodes = sorter.TopologicalSort(graph);
+
+            Assert.AreSame(node, nodes.Single());
+        }
+
+        [Test]
+        public void ShouldSortMultipleNodesGraph()
+        {
+            var graph = new Graph<char>();
+            graph.AddNodes('u', 'v', 'w', 'x', 'y', 'z');
+            graph.AddEdge('v', 'y');
+            graph.AddEdge('u', 'v');
+            graph.AddEdge('u', 'x');
+            graph.AddEdge('y', 'x');
+            graph.AddEdge('x', 'v');
+            graph.AddEdge('z', 'z');
+            graph.AddEdge('w', 'z');
+            graph.AddEdge('w', 'y');
+
+            var nodes = sorter.TopologicalSort(graph);
+
+            var expected = "wzuvyx";
+            var actual = new string(nodes.Select(n => n.Value).ToArray());
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Algorithms/Graphs/TopologicalSorter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Graphs
+{
+    public class TopologicalSorter
+    {
+        public IEnumerable<Node<T>> TopologicalSort<T>(Graph<T> graph)
+        {
+            var visitedNodes = new HashSet<Node<T>>();
+            var processedNodes = new Stack<Node<T>>();
+
+            foreach (var node in graph.Nodes)
+            {
+                if (!visitedNodes.Contains(node))
+                {
+                    Visit(node, visitedNodes, processedNodes);
+                }
+            }
+
+            return processedNodes;
+        }
+
+        private void Visit<T>(Node<T> node, ISet<Node<T>> visitedNodes, Stack<Node<T>> processedNodes)
+        {
+            visitedNodes.Add(node);
+
+            foreach (var neighbour in node.Neighbours)
+            {
+                if (!visitedNodes.Contains(neighbour))
+                {
+                    Visit(neighbour, visitedNodes, processedNodes);
+                }
+            }
+
+            processedNodes.Push(node);
+        }
+    }
+}
